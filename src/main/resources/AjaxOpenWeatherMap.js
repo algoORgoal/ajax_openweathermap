1d5cc53b7713b41a9cc8f45dc66750a0d68f41cc
algoORgoal <qufstar46@gmail.com>
2019-08-19 18:02:55 +0900

    create js

--- src/main/resources/AjaxOpenWeatherMap.js
+++ src/main/resources/AjaxOpenWeatherMap.js
@@ -1,11 +1,13 @@
 $(function () {
     $.get("http://localhost:8080/remote/openweather", function (data) {
+        $.map(data["list"], function (index10, value10) {
+            console.log("typeof index10: " + typeof index10);
+            console.log("typeof value10: " + typeof value10);
+        });
+        console.log("-------------------------------------------");
         $.each(data["list"], function (index, value2) {
-            $.map(value2, function (value3, currentData2) {
-
-                $("#time1").append(value2["main"]["temp"]);
-                console.log()
-            })
+            console.log("typeof index: " + typeof index);
+            console.log("typeof value2: " + typeof value2);
         });
     });
 });
